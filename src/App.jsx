6e206d06fdc1ef78f1b3086e6490d41c11d2cdce
bucad-andrey <bucad.andrey.g@gmail.com>
2026-03-01 23:15:11 +0800

    Routing Logic is working, redirection of URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import CreateForm from "./components/sender/CreateForm";
 import EventForm from "./components/sender/EventForm";
+import ReceiverUI from "./components/receiver/RecieverUI";
 import { useAuth } from "./components/hooks/userAuth";
-import { SpeedInsights } from '@vercel/speed-insights/react';
-
 
 function App() {
   const { user, loading } = useAuth();
 
-  if (loading) return <div className="text-white text-2xl font-bold flex items-center justify-center min-h-screen">Loading...</div>;
-  <SpeedInsights/>
-  return user ? <EventForm /> : <CreateForm />;
-  
+  if (loading) {
+    return (
+      <div className="text-white text-2xl font-bold flex items-center justify-center min-h-screen">
+        Loading...
+      </div>
+    );
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* Receiver Route */}
+        <Route path="/love/:giftId/*" element={<ReceiverUI />} />
+
+        {/* Sender Routes */}
+        <Route
+          path="/*"
+          element={user ? <EventForm /> : <CreateForm />}
+        />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/sender/EventForm.jsx
+++ src/components/sender/EventForm.jsx
@@ -1,14 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from "../ui/Header";
 import MainContent from "../ui/MainContent";
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function EventForm() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
       <MainContent />
-    </BrowserRouter>
+    </>
   );
 }
 
