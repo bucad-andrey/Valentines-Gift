d1cedc8abe6d697e040c4b89e7f740add228d385
bucad-andrey <bucad.andrey.g@gmail.com>
2026-04-25 15:43:21 +0800

    EventForm is Added Back

---
+++ src/components/sender/EventForm.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Header from "../ui/Header";
+import MainContent from "../ui/MainContent";
+
+function EventForm() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+    </>
+  );
+}
+
+export default EventForm;
